7141a8b906cc0767ab7b3b0870f64db8c2a0f5bb
IsaacThaJunior <isaacjunez669@gmail.com>
2022-09-01 19:13:07 +0100

    created a new React app for learning purposes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [first, setFirst] = useState('');
+
+	const handleClick = (e) => {
+		setFirst(e.target.value);
+	};
+
+	const clear = () => {
+		setFirst('');
+	};
+
+	return (
+		<div className="App">
+			<input type="text" value={first} onChange={handleClick} />
+			<button onClick={clear}>Clear Input</button>
+		</div>
+	);
 }
 
 export default App;
