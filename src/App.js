9ccb4ac8d117cf395e1d0915052c0df383481a0a
IsaacThaJunior <isaacjunez669@gmail.com>
2022-09-09 20:27:01 +0100

    useId and forms in react

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import './App.css';
 
 function App() {
 	const firstId = useId();
-	const secondId = useId()
+	const secondId = useId();
 
-	return <div className="App">
-		<h3>The first id generated is {firstId}</h3>
-		<h3>The second id generated is {secondId} and different from {firstId} </h3>
-	</div>;
+	return (
+		<div className="App">
+			<form>
+				<div>
+					<label htmlFor={`${firstId}-email`}>Email</label>
+					<input id={`${firstId}-email`} type="text" placeholder="Your email" />
+				</div>
+				<div>
+					<label htmlFor={`${secondId}-password`}>Password</label>
+					<input
+						id={`${secondId}-password`}
+						type="password"
+						placeholder="Your password"
+					/>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default App;
